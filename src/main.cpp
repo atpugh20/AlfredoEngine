cb1ed24e735ca4a14923b6d501e5075d336b716b
Alex <133706872+atpugh20@users.noreply.github.com>
2025-01-17 17:01:35 -0800

    fixed textures, added camera movement

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 #include "VertexBuffer.h"
 #include "ElementBuffer.h"
 
+#include <cmath>
+
 const char* vsSource = R"(
 #version 460 core
 layout (location = 0) in vec3 aPos;
@@ -21,12 +23,10 @@ layout (location = 2) in vec2 aTexCoord;
 out vec3 vColor;
 out vec2 TexCoord;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 mvp;
 
 void main() {
-    gl_Position =  projection * view * model * vec4(aPos, 1.0);
+    gl_Position =  mvp * vec4(aPos, 1.0);
     vColor = aColor;
     TexCoord = aTexCoord;
 }
@@ -46,6 +46,25 @@ void main() {
 }
 )";
 
+glm::vec3 cameraPos     = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront   = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp      = glm::vec3(0.0f, 1.0f,  0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+void processInput(GLFWwindow *window) {
+    const float cameraSpeed = 5.0f * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
 int main(void) {
         
     const float w_W = 800.0f;
@@ -63,25 +82,47 @@ int main(void) {
 
     // Vertex and index Data
     float vertices[] = {
-        // Positions        // Colors           // Texture coords
+        // Positions       // Colors           // Texture coords
+
+        // front
         -off,  off,  off,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f,         // Top left
          off,  off,  off,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f,         // Top right
          off, -off,  off,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f,         // Bot right
         -off, -off,  off,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f,         // Bot left
-
+        // back
+         off,  off, -off,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f,         // Top left
+        -off,  off, -off,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f,         // Top right
+        -off, -off, -off,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f,         // Bot right
+         off, -off, -off,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f,         // Bot left
+        // left
+        -off,  off, -off,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f,         // Top left
+        -off,  off,  off,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f,         // Top right
+        -off, -off,  off,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f,         // Bot right
+        -off, -off, -off,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f,         // Bot left        
+        // right
+         off,  off,  off,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f,         // Top left
+         off,  off, -off,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f,         // Top right
+         off, -off, -off,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f,         // Bot right
+         off, -off,  off,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f,         // Bot left
+        // top
         -off,  off, -off,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f,         // Top left
          off,  off, -off,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f,         // Top right
+         off,  off,  off,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f,         // Bot right
+        -off,  off,  off,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f,         // Bot left
+        // bottom
+        -off, -off,  off,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f,         // Top left
+         off, -off,  off,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f,         // Top right
          off, -off, -off,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f,         // Bot right
         -off, -off, -off,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f,         // Bot left
     };
 
     int indices[] = {
-        0, 1, 2,    2, 3, 0,  // front
-        4, 5, 6,    6, 7, 4,  // back
-        4, 0, 3,    3, 7, 4,  // left
-        1, 5, 6,    6, 2, 1,  // right
-        4, 5, 1,    1, 0, 4,  // top
-        7, 6, 2,    2, 3, 7,  // bottom
+        0,  1,  2,     2,  3,  0,  // front
+        4,  5,  6,     6,  7,  4,  // back
+        8,  9, 10,    10, 11,  8,  // left
+       12, 13, 14,    14, 15, 12,  // right
+       16, 17, 18,    18, 19, 16,  // top
+       20, 21, 22,    22, 23, 20,  // bottom
     };
     const int iCount = sizeof(indices) / sizeof(indices[0]);
 
@@ -101,7 +142,7 @@ int main(void) {
     VertexArray* VAO = new VertexArray();
     VertexBuffer* VBO = new VertexBuffer();
     ElementBuffer *EBO = new ElementBuffer();
-    Texture *texture = new Texture("res/textures/torchic.png");
+    Texture *texture = new Texture("res/textures/diamond.png");
     Shader *shader = new Shader(vsSource, fsSource);
     
     // Bind everything to GPU
@@ -122,28 +163,45 @@ int main(void) {
     // Texture attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, floatsPerVertex * sizeof(float), (void *)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
-
     
+    // Camera
+
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 projection;
+    glm::mat4 mvp;
+
+    const float radius = 20.0f;
+    float camX;
+    float camZ;
+
+    // Rotation of cubes
     float degrees = 0.1f;
 
-    while (!glfwWindowShouldClose(window)) { 
-        degrees += 0.1f;
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        glm::mat4 view = glm::mat4(1.0f);
-        glm::mat4 projection;
+    while (!glfwWindowShouldClose(window)) {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        processInput(window);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        degrees += 0.1f;
+        //cameraPos.z += 0.01;
+        camX = sin(glfwGetTime()) * radius;
+        camZ = cos(glfwGetTime()) * radius;
+
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), w_W / w_H, 0.1f, 100.0f);
 
-        shader->setMat4("view", view);
-        shader->setMat4("projection", projection);
-
         for (unsigned int i = 0; i < 10; i++) {
-            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             model = glm::rotate(model, glm::radians(20.0f * i + degrees), glm::vec3(1.0f, 0.3f, 0.5f));  
-            shader->setMat4("model", model);
+            mvp = projection * view * model;
+            shader->setMat4("mvp", mvp);
             glDrawElements(GL_TRIANGLES, iCount, GL_UNSIGNED_INT, 0);
         }
          
