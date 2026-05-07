686859a016b0cb9d60bd11aebd410e20181670a0
Alex <133706872+atpugh20@users.noreply.github.com>
2026-05-06 22:25:13 -0700

    RESTART

--- include/Vertex.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct Vertex {
-    float positions[3];
-    float colors[4];
-    float textureCoords[2];
-};
--- include/macros.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-#define GLEW_STATIC
-
-#define print(x) std::cout << x << std::endl;
-
-#define printError(x) std::cerr << x << std::endl;
